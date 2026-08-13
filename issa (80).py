b147369e32486c1d573c1ec70f1299f3f0b0279b
issayoume2012-ui <issayoume2012@gmail.com>
2026-08-13 10:46:29 +0000

    Update issa (80).py

--- issa (80).py
+++ issa (80).py
@@ -2965,7 +2965,7 @@ elif st.session_state.espace_actif == "🔒 Espace Administration (Sécurisé)":
             )
             sauvegarder_lot_vers_supabase("eleves_db", st.session_state.eleves_db, on_conflict_cols="id")
 
-              st.success(f"✅ Élève {nc_el} inscrit et enregistré sur Supabase !")
+            st.success(f"✅ Élève {nc_el} inscrit et enregistré sur Supabase !")
             st.rerun()
 
       st.markdown("---")
